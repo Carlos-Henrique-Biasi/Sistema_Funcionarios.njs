a63369e1b608c5ae8b28a6730cc50cdb8700d959
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-08-18 11:58:11 -0300

    Update faturamentoControllers.js

--- controllers/faturamentoControllers.js
+++ controllers/faturamentoControllers.js
@@ -4,12 +4,13 @@ const gravarFaturamento = async (req,res) =>{
     const {empresa_id} = req.params
     const {faturamento} = req.body
 
-    if(faturamento <= 0){
+    if(faturamento <= 0 || /*regra nan*/ || faturamento.trim().length == 0){
         return res.status(400).json({ erro: "Digite um valor válido."})
     }
 
     try{
        
+
     }catch(erro){
         console.log(erro)
         res.status(500).json({ erro: "Erro ao cadastrar faturamento." })

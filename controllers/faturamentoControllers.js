27e9eb613a6700407d1a05f8fa779431c056467c
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-08-19 11:38:52 -0300

    Update faturamentoControllers.js

--- controllers/faturamentoControllers.js
+++ controllers/faturamentoControllers.js
@@ -29,6 +29,8 @@ const editarFaturamento = async (req,res) =>{
     const {id, empresa_id} = req.params
     const {valor, motivo, data} = req.body
     const valorConvertido = Number(valor)
+
+    //função de pegar 1 faturamento
     
     if (motivo && String(motivo).trim() !== "") {  // so valida se tiver campo preenchido, já que é opicional
         if (!Number.isNaN(Number(motivo))) {
@@ -45,15 +47,46 @@ const editarFaturamento = async (req,res) =>{
     }
 
     try{
-
+        //codigo para enviar ao mysql
     }catch(erro){
-
+        //codigo de erro ao se conectar
     }
 }
 const excluirFaturamento = async (req,res) =>{
-    
+    const {id, empresa_id} = req.params
+    //funcao de pegar 1 faturamento
+
 }
 
 const listarFaturamento = async (req,res) =>{
     
+}
+
+const pegar1Faturamento = async (req,res) =>{
+    const {empresa_id, id} = req.params
+    
+    try{
+        const [faturamento] = await db.query(
+            'SELECT * FROM faturamento WHERE empresa_id = ? AND id = ?', 
+            [empresa_id, id]
+        )
+
+        if(faturamento.length === 0){
+            return res.status(404).json({ erro: "Faturamento não encontardo, ou não pertencente a empresa."})
+        }
+
+        res.json(faturamento[0])
+        
+    }catch(erro){
+        console.log(erro)
+        res.status(500).json({ erro: "Erro ao buscar os faturamento." })
+    }
+}
+
+module.exports = {
+    gravarFaturamento,
+    editarFaturamento,
+    excluirFaturamento,
+    listarFaturamento,
+    pegar1Faturamento
 }

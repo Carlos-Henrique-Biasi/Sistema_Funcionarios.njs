cf3faa33c65f1c1943f8a75c4628c0c5d85e676f
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-08-19 11:55:14 -0300

    Update faturamentoControllers.js

--- controllers/faturamentoControllers.js
+++ controllers/faturamentoControllers.js
@@ -13,12 +13,12 @@ const gravarFaturamento = async (req,res) =>{
             return res.status(400).json({ erro: "Digite um motivo diferente de um número." });
         }
     }
-    if(data == undefined){
-        return res.status(400).json({ erro: "Digite um valor válido."})
+    if(data == undefined || new Date(data) > new Date() || Number.isNaN(new Date(data).getTime())){ //getTime() é tipo um number, que verifica o formato e se não estiver em formato de data, devolve NaN
+        return res.status(400).json({ erro: "Escolha um valor válido."})
     }
 
     try{
-
+        //codigo mysql
     }catch(erro){
         console.log(erro)
         res.status(500).json({ erro: "Erro ao cadastrar faturamento." })
@@ -30,8 +30,6 @@ const editarFaturamento = async (req,res) =>{
     const {valor, motivo, data} = req.body
     const valorConvertido = Number(valor)
 
-    //função de pegar 1 faturamento
-    
     if (motivo && String(motivo).trim() !== "") {  // so valida se tiver campo preenchido, já que é opicional
         if (!Number.isNaN(Number(motivo))) {
             return res.status(400).json({ erro: "Digite um motivo diferente de um número." });
@@ -41,9 +39,9 @@ const editarFaturamento = async (req,res) =>{
     if(
         (valorConvertido == undefined || Number.isNaN(valorConvertido) || valorConvertido <= 0) && 
         (motivo == undefined || motivo.trim().length == 0) &&
-        (data == undefined)
+        (data == undefined || new Date(data) > new Date() || Number.isNaN(new Date(data).getTime()))
     ){
-        return res.status(400).json({ erro : "Algum valor deve ser editado com valores válidos."})
+        return res.status(400).json({ erro : "Algum campo deve ser editado com valores válidos."})
     }
 
     try{
@@ -54,7 +52,6 @@ const editarFaturamento = async (req,res) =>{
 }
 const excluirFaturamento = async (req,res) =>{
     const {id, empresa_id} = req.params
-    //funcao de pegar 1 faturamento
 
 }
 

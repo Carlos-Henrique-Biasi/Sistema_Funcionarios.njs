be1229574788dfc1704ebfdd63e5d85d5f730591
Carlos-Henrique-Biasi <carloshbiasicuba@gmail.com>
2026-08-25 10:12:35 -0300

    .

--- front-end/scripts/faturamento.js
+++ front-end/scripts/faturamento.js
@@ -1,5 +1,5 @@
 //======================================= BOTÕES DE FATURAMENTO ===================================
-const botaoBuscarF = document.getElementById('btnBuscaFat')
+const botaoBuscarF = document.getElementById('btnBuscarFat')
 const botaoCadastrarF = document.getElementById('btnCadastrarFat')
 const botaoBuscarIDF = document.getElementById('btnBuscarIdFat') // busca primeiro para depois gerar os elementos dinâmicos
 const botaoBuscarIDF2 = document.getElementById('btnVExcluirFat') // Mesma coisa porém com exclusão
@@ -7,11 +7,42 @@ const botaoResultadoF = document.getElementById('btnTotalFat')
 const botaoBuscarFS = document.getElementById('btnListarFat')
 //======================================= BOTÕES DE FATURAMENTO ===================================
 
+//=================================== ELEMENTOS DE FATURAMENTO ====================================
+
+//BUSCAR FATURAMENTO
+const fatValor = document.getElementById("fatValor")
+const fatMotivo= document.getElementById("fatMotivo")
+const fatData= document.getElementById("fatData")
+//BUSCAR FATURAMENTO
+
+//CADASTRAR FATURAMENTO
+const msgCadastroFat = document.getElementById("msgCadFat")
+//CADASTRAR FATURAMENTO
+
+//ATUALIZAR FATURAMENTO
+const infoAtualizarFat = document.getElementById("infoAtualizarFat")
+//ATUALIZAR FATURAMENTO
+
+//EXCLUIR FATURAMENTO
+const divExcluirFat = document.getElementById("divExcluirFat")
+//EXCLUIR FATURAMENTO
+
+//RESULTADO DE FATURAMENTO
+const qntregistros = document.getElementById("infoQtdFat")
+const valorTotal = document.getElementById("infoTotalFat")
+//RESULTADO DE FATURAMENTO
+
+//LISTAR FATURAMENTO
+const listaFaturamento = document.getElementById("listaFat")
+//LISTAR FATURAMENTO
+
+//=================================== ELEMENTOS DE FATURAMENTO ====================================
+
 //================================ AÇÕES DOS BOTÕES DE FATURAMENTO ================================
 botaoBuscarF.addEventListener("click", buscarFaturamento)
 botaoCadastrarF.addEventListener("click", cadastrarFaturamento)
 botaoBuscarIDF.addEventListener("click", buscarFaturamentoAtualizacao)
 botaoBuscarIDF2.addEventListener("click", buscarFaturamentoExclusao)
 botaoResultadoF.addEventListener("click", resultadoFaturamento)
-botaoBuscarFS.addEventListener("clcik", listarFaturamentos)
+botaoBuscarFS.addEventListener("click", listarFaturamentos)
 //================================ AÇÕES DOS BOTÕES DE FATURAMENTO ================================
